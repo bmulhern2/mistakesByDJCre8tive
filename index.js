cbb961ede8cd5643015cc0d66c6b3da83205f515
bmulhern2 <bmulhern09@gmail.com>
2020-01-05 03:27:30 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000);
+module.export = app;
